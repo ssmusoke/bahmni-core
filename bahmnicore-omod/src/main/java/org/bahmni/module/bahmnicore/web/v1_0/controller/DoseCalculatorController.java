49dc04798858b324d2289814c11a78b8a9c0a1e0
Alaggesan Palani <alagesann@gmail.com>
2016-07-31 15:26:08 +0530

    Refactoring rulesengine

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
@@ -6,6 +6,7 @@ import org.openmrs.module.rulesengine.rule.BSABasedDoseRule;
 import org.openmrs.module.rulesengine.rule.WeightBasedDoseRule;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/calculateDose")
 public class DoseCalculatorController extends BaseRestController {
+
+    @Autowired
+    private RulesEngine rulesEgine;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
